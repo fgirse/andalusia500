41d2a72951978d85e8ca5ae7eb4899a892ad76c9
Frank Girse <fgirse@mac-mini-von-frank.home>
2023-07-03 07:02:50 +0200

    4er commit

--- app/page.js
+++ app/page.js
@@ -4,8 +4,8 @@ export default function Home() {
   return (
    <main className="bg-hero w-[100vw] bg-[url('/assets/images/images.webp')] bg-no-repeat flex min-h-screen flex-col items-center justify-between p-24">
         <div className={lontoutline.className}>
-        <h1 className="text-left bg-white/40 text-5xl sm:text-8xl font-bold tracking-tight lg:-mt-[5vh] lg:text-[7rem] z-20">
-    Wohntraum {" "}
+        <h1 className="text-left bg-white/40 text-5xl sm:text-8xl font-bold tracking-tight lg:px-3 lg:py-1 lg:bg-slate-800/20 lg:-mt-[7vh] lg:text-[7rem] z-20">
+    Wohntraum {" "}<br />
     <span className="bg-gradient-to-r from-[#ff781f] to-[#ffaf7a] text-transparent bg-clip-text">
         Andalusia
     </span>
